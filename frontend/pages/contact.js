ad8f72b6c9ab4cfdd0e960f7c4612d5b4026779b
Sayan <sayanmunshi36@gmail.com>
2022-05-26 17:26:55 +0530

    Contact page form functionality added

--- frontend/pages/contact.js
+++ frontend/pages/contact.js
@@ -21,62 +21,83 @@ const Contact = () => {
                 est? Porro omnis facilis dolor libero.
               </p>
             </div>
-            <div className="lg:w-1/2 md:w-2/3 mx-auto">
-              <div className="flex flex-wrap -m-2">
-                <div className="p-2 w-1/2">
-                  <div className="relative">
-                    <label
-                      htmlFor="name"
-                      className="leading-7 text-base sm:text-lg font-bangers tracking-wide"
-                    >
-                      Name
-                    </label>
-                    <input
-                      type="text"
-                      id="name"
-                      name="name"
-                      className="w-full bg-gray-700 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
+            <form
+              action="https://formsubmit.co/97118c8fd53b2d5c96ee5621605b4886"
+              method="POST"
+            >
+              <input
+                type="hidden"
+                name="_autoresponse"
+                value="Thanks for contacting with us"
+              />
+              <input
+                type="hidden"
+                name="_next"
+                value={`${process.env.NEXT_PUBLIC_URL}/contact`}
+              />
+              <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                <div className="flex flex-wrap -m-2">
+                  <div className="p-2 w-1/2">
+                    <div className="relative">
+                      <label
+                        htmlFor="name"
+                        className="leading-7 text-base sm:text-lg font-bangers tracking-wide"
+                      >
+                        Name
+                      </label>
+                      <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        className="w-full bg-gray-700 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        required
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="p-2 w-1/2">
-                  <div className="relative">
-                    <label
-                      htmlFor="email"
-                      className="leading-7 text-base sm:text-lg font-bangers tracking-wide"
-                    >
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      className="w-full bg-gray-700 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
+                  <div className="p-2 w-1/2">
+                    <div className="relative">
+                      <label
+                        htmlFor="email"
+                        className="leading-7 text-base sm:text-lg font-bangers tracking-wide"
+                      >
+                        Email
+                      </label>
+                      <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        className="w-full bg-gray-700 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        required
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="p-2 w-full">
-                  <div className="relative">
-                    <label
-                      htmlFor="message"
-                      className="leading-7 text-base sm:text-lg font-bangers tracking-wide"
-                    >
-                      Message
-                    </label>
-                    <textarea
-                      id="message"
-                      name="message"
-                      className="w-full bg-gray-700 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 leading-6 transition-colors duration-200 ease-in-out resize-none"
-                    ></textarea>
+                  <div className="p-2 w-full">
+                    <div className="relative">
+                      <label
+                        htmlFor="message"
+                        className="leading-7 text-base sm:text-lg font-bangers tracking-wide"
+                      >
+                        Message
+                      </label>
+                      <textarea
+                        id="message"
+                        name="message"
+                        className="w-full bg-gray-700 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 leading-6 transition-colors duration-200 ease-in-out resize-none"
+                        required
+                      ></textarea>
+                    </div>
                   </div>
-                </div>
-                <div className="p-2 w-full">
-                  <button className="btn btn-primary flex mx-auto border-0 py-2 px-4 sm:px-8 text-base sm:text-lg">
-                    Submit
-                  </button>
+                  <div className="p-2 w-full">
+                    <button
+                      type="submit"
+                      className="btn btn-primary flex mx-auto border-0 py-2 px-4 sm:px-8 text-base sm:text-lg"
+                    >
+                      Submit
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
+            </form>
           </div>
         </section>
       </div>
